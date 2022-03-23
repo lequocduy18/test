c1df39e10459d5e50808aafec82e2ab7e38170c9
lequocduy18 <68944776+lequocduy18@users.noreply.github.com>
2022-03-23 07:56:22 +0700

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
 const categoryRoutes = require("./routes/category");
 const productRoutes = require("./routes/product");
-const braintreeRoutes = require("./routes/braintree");
 const orderRoutes = require("./routes/order");
 
 // app
